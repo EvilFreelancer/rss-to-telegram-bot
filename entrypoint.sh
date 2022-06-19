891ad03d2e5baf7ddcff08f45995e2ccab6bd5c1
Paul Rock <paul@drteam.rocks>
2022-06-19 22:54:49 +0300

    Main script enabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 TS_TIMEOUT="${TS_TIMEOUT:-3600}"
 
 # If directory is not set, then use defaults
-RSSREADER_CACHE_DIR="${RSSREADER_CACHE_DIR:-/tmp/.rssreader_cache}"
+RSSREADER_CACHE_DIR="${RSSREADER_CACHE_DIR:-/app/cache}"
 
 # Change ownership to user=nobody, group=nobody
 chown -R nobody:nobody "$RSSREADER_CACHE_DIR"
 
 # Run infinity loop
 while true; do
-  #su -s /bin/bash -c "php -f /app/rss-to-telegram.php" nobody
+  su -s /bin/bash -c "php -f /app/rss-to-telegram.php" nobody
   sleep $TS_TIMEOUT
 done
